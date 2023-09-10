5380925d8c3194b61f88cbbe794c459749bb7e38
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-09-10 11:50:59 +0100

    i just started, let's have fun

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+
+function App(): JSX.Element {
+
+  return (
+    <SafeAreaView >
+      <View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
