64843137cec243fa5c8235ab73270f49320837bd
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-12-04 21:55:22 +0100

    let's work on the app again

--- App.tsx
+++ App.tsx
@@ -5,24 +5,16 @@ import {
   Text
 } from 'react-native';
 
-import { Provider } from 'react-redux';
 import tw from 'tailwind-react-native-classnames';
-import { store } from './store';
-import { useSelector } from 'react-redux';
 
 function App(): JSX.Element {
 
-  const { test } = useSelector((state: any) => state.test)
-
   return (
-    <Provider store={store}>
-      <SafeAreaView >
-      <View style={tw`w-full h-full bg-red-500 flex justify-center items-center`}>
-      <Text>Ahermomou rides {test}</Text>
+      <SafeAreaView>
+      <View style={tw`w-full h-full flex justify-center items-center`}>
+      <Text>Ahermomou rides</Text>
       </View>
       </SafeAreaView>
-    </Provider>
-      
   );
 }
 
